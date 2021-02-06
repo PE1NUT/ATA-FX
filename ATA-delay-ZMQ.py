9b47c76ba346b1002215cb17682db9cca91b45f5
Paul Boven <p.boven@xs4all.nl>
2021-02-06 14:57:21 +0100

    Use the ECEF antenna coordinates

--- ATA-delay-ZMQ.py
+++ ATA-delay-ZMQ.py
@@ -19,38 +19,22 @@ import pmt
 import zmq
 import sys
 
+# Read all the source coordinates
+ant_pos = {};
+coords_file = open('antenna_coordinates_ecef.txt', 'r')
+for line in coords_file:
+    if line[0:7] == 'Antenna':
+        continue
+    (ant, x, y, z) = line.split(',')
+    ant = ant.lower()
+    ant_pos[ant] = (float(x), float(y),float(z))
+
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind("tcp://127.0.0.1:50001")
 
-# Note: These coordinates are not official, but from clicking on Google Maps.
-ant1a = EarthLocation.from_geodetic(lat = 40.816695 * u.deg, \
-                                    lon = -121.471042 * u.deg, \
-                                    height = 986 * u.m)
-
-ant1c = EarthLocation.from_geodetic(lat = 40.815967 * u.deg, \
-                                    lon = -121.470725 * u.deg, \
-                                    height = 986 * u.m)
-
-ant1h = EarthLocation.from_geodetic(lat = 40.816410 * u.deg, \
-                                    lon = -121.471831 * u.deg, \
-                                    height = 986 * u.m)
-
-ant4g = EarthLocation.from_geodetic(lat = 40.8183207 * u.deg, \
-                                    lon = -121.4704345 * u.deg, \
-                                    height = 986 * u.m)
-
-# Unfortunately, EarthLocation doesn't override the - operator.
-# These are corrections for fringe stopping on each baseline, which can't
-# readily be attributed to any antenna.
-baseline = {}
-baseline[('1a', '4g')] = EarthLocation(ant1a.x - ant4g.x -0.31*u.m, ant1a.y - ant4g.y + 0.12 *u.m, ant1a.z - ant4g.z + 0.38 * u.m)
-baseline[('1c', '4g')] = EarthLocation(ant1c.x - ant4g.x -1.038*u.m, ant1c.y - ant4g.y - 0.697 *u.m, ant1c.z - ant4g.z + 1.63 * u.m)
-baseline[('1h', '4g')] = EarthLocation(ant1h.x - ant4g.x -0.10*u.m, ant1h.y - ant4g.y + 0.009 *u.m, ant1h.z - ant4g.z + 0.42 * u.m)
-baseline[('2b', '4g')] = EarthLocation(39.922913 * u.m, 60.929134 * u.m, 83.856601 * u.m)
-
 # Note: this number is still being refined, and depends on the baseline.
-# NOte: it can jump by 100ns due to PPS/10MHz phase drift
+# Note: it can jump by 100ns due to PPS/10MHz phase drift
 fixed_delay = {}
 fixed_delay[('1a', '4g')] = -750.8 *u.ns
 fixed_delay[('1c', '4g')] = -750.8 *u.ns - 147.2  * u.ns
@@ -58,14 +42,18 @@ fixed_delay[('1h', '4g')] = -750.8 *u.ns + 26.6 * u.ns
 fixed_delay[('2b', '4g')] = 33.46 * 20 * u.ns
 
 source = SkyCoord(ra = float(sys.argv[1]) * u.deg, dec = float(sys.argv[2]) * u.deg)
-anta = sys.argv[3]
-antb = sys.argv[4]
+ant_a = sys.argv[3].lower()
+ant_b = sys.argv[4].lower()
+
+baseline = EarthLocation((ant_pos[ant_b][0] - ant_pos[ant_a][0]) * u.m,
+                         (ant_pos[ant_b][1] - ant_pos[ant_b][1]) * u.m,
+                         (ant_pos[ant_b][2] - ant_pos[ant_b][2]) * u.m)
 
 while True:
     obstime = Time.now()
     baseline_projected = \
-        baseline[(antb, anta)].get_gcrs(obstime).cartesian.xyz.T.dot(source.cartesian.xyz)
-    delay = baseline_projected / c + fixed_delay[(antb, anta)]
+        baseline.get_gcrs(obstime).cartesian.xyz.T.dot(source.cartesian.xyz)
+    delay = baseline_projected / c + fixed_delay[(ant_b, ant_a)]
     msg = pmt.cons(pmt.intern("delay"), pmt.from_double(delay.value))
     sb = pmt.serialize_str(msg)
     socket.send(sb)
